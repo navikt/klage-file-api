be9a98a6ab588bbad7dd8daa52db1dc5afc361f5
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-07-10 13:17:52 +0200

    Separate point for getting signed url. Point for getting blob.

--- src/main/kotlin/no/nav/klage/service/KlageService.kt
+++ src/main/kotlin/no/nav/klage/service/KlageService.kt
@@ -2,32 +2,31 @@ package no.nav.klage.service
 
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.BlobInfo
-import com.google.cloud.storage.StorageOptions
+import com.google.cloud.storage.Storage
 import no.nav.klage.getLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.ByteArrayResource
-
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.FileNotFoundException
-import java.util.*
 
 @Service
-class KlageService {
+class KlageService(
+    private val gcsStorage: Storage,
+    @Value("\${GCS_BUCKET}")
+    private var bucket: String
+)  {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
         private val logger = getLogger(javaClass.enclosingClass)
     }
 
-    @Value("\${GCS_BUCKET}")
-    private lateinit var bucket: String
-
     fun getKlageAsResource(id: String): Resource {
         logger.debug("Getting klage with id {}", id)
 
-        val blob = getGcsStorage().get(bucket, id.toPath())
+        val blob = gcsStorage.get(bucket, id.toPath())
         if (blob == null || !blob.exists()) {
             logger.warn("Klage not found: {}", id)
             throw FileNotFoundException()
@@ -38,7 +37,7 @@ class KlageService {
 
     fun deleteKlage(id: String): Boolean {
         logger.debug("Deleting klage with id {}", id)
-        return getGcsStorage().delete(BlobId.of(bucket, id.toPath())).also {
+        return gcsStorage.delete(BlobId.of(bucket, id.toPath())).also {
             if (it) {
                 logger.debug("Klage was deleted.")
             } else {
@@ -51,7 +50,7 @@ class KlageService {
         logger.debug("Saving klage")
 
         val blobInfo = BlobInfo.newBuilder(BlobId.of(bucket, id.toPath())).build()
-        val result = getGcsStorage().create(blobInfo, file.bytes).exists()
+        val result = gcsStorage.create(blobInfo, file.bytes).exists()
 
         logger.debug("Klage saved, and id is {}", id)
 
@@ -59,7 +58,4 @@ class KlageService {
     }
 
     private fun String.toPath() = "klage/$this"
-
-    private fun getGcsStorage() = StorageOptions.getDefaultInstance().service
-
 }
