359f05c90942e2c2900121bc6b790596647225b6
Andreas Jonsson <andreas.jonsson@nav.no>
2026-02-24 12:57:30 +0100

    Support larger files. Try not to read into memory.

--- src/main/kotlin/no/nav/klage/service/AttachmentService.kt
+++ src/main/kotlin/no/nav/klage/service/AttachmentService.kt
@@ -5,12 +5,12 @@ import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.StorageOptions
 import no.nav.klage.getLogger
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.io.ByteArrayResource
-
+import org.springframework.core.io.InputStreamResource
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.FileNotFoundException
+import java.nio.channels.Channels
 import java.util.*
 
 @Service
@@ -33,7 +33,7 @@ class AttachmentService {
             throw FileNotFoundException()
         }
 
-        return ByteArrayResource(blob.getContent())
+        return InputStreamResource(Channels.newInputStream(blob.reader()))
     }
 
     fun deleteAttachment(id: String): Boolean {
