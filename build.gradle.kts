6791d1530ae2bf5519a88abb2a8a72b81bcf9509
Andreas Jonsson <andreas.jonsson@nav.no>
2026-05-05 08:16:34 +0200

    Remove overrides since Spring has updated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,6 @@ plugins {
     idea
 }
 
-// CVE GHSA-72hv-8253-57qq: jackson-core async parser DoS. Remove when Spring has updated.
-extra["jackson-2-bom.version"] = "2.21.1"
-extra["jackson-bom.version"] = "3.1.0"
-
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 apply(plugin = "io.spring.dependency-management")
